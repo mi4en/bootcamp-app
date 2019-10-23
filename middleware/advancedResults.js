ec77fc419fac3c09f9f197e1a4eb84593f8d2e7f
mario_nikolov <mnikolov@rnd-solutions.net>
2019-10-23 15:03:51 +0300

    extract advancedResults to middleware; add advancedResults to courses;

--- middleware/advancedResults.js
+++ middleware/advancedResults.js
@@ -2,30 +2,30 @@ const advancedResults = (model, populate) => async (req, res, next) => {
   let query;
 
   // Copy req.query
-  const reqQuery = { ...req.query };
+  let reqQuery = { ...req.query };
 
   // Fields to exclude
   const removeFields = ['select', 'sort', 'page', 'limit'];
 
-  // Loop over removeFields and delete them from reqQuery
+  //Loop over removeFields and delete them from reqQuery
   removeFields.forEach(param => delete reqQuery[param]);
 
   // Create query string
   let queryStr = JSON.stringify(reqQuery);
 
-  // Create operators ($gt, $gte, etc)
+  // Create mongoose operators ()$ge, $gte etc
   queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
 
   // Finding resource
   query = model.find(JSON.parse(queryStr));
 
-  // Select Fields
+  // Select fields
   if (req.query.select) {
     const fields = req.query.select.split(',').join(' ');
     query = query.select(fields);
   }
 
-  // Sort
+  // Sort fields
   if (req.query.sort) {
     const sortBy = req.query.sort.split(',').join(' ');
     query = query.sort(sortBy);
@@ -49,7 +49,7 @@ const advancedResults = (model, populate) => async (req, res, next) => {
   // Executing query
   const results = await query;
 
-  // Pagination result
+  // Pagination Result
   const pagination = {};
 
   if (endIndex < total) {
