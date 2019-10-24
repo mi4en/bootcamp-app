860ef568fcfea724276c823ee0c758084a8d1cc6
mario_nikolov <mnikolov@rnd-solutions.net>
2019-10-24 15:26:50 +0300

    add review model; add review routes; add review ctrl; add logout route; add destroy cookie with token;

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,15 +7,17 @@ const User = require('../models/User');
 exports.protect = asyncHandler(async (req, res, next) => {
   let token;
 
+  // Set token from Bearer token in header
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
   ) {
     token = req.headers.authorization.split(' ')[1];
   }
-  //   else if (req.cookies.token) {
-  //     token = req.cookis.token;
-  //   }
+  // Set token form cookie
+  // else if (req.cookies.token) {
+  //   token = req.cookies.token;
+  // }
 
   // Make sure token exists
   if (!token) {
