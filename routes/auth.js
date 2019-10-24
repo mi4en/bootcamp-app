3672da13fb1271172dfe9a2851c8bf8f7794e307
mario_nikolov <mnikolov@rnd-solutions.net>
2019-10-24 11:38:38 +0300

    add update password and update details to users;

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,23 @@
 const express = require('express');
+const { protect } = require('../middleware/auth');
 const {
   register,
   login,
   getMe,
-  forgotPassword
+  updateDetails,
+  updatePassword,
+  forgotPassword,
+  resetPassword
 } = require('../controllers/auth');
-const { protect } = require('../middleware/auth');
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
+router.put('/updatedetails', protect, updateDetails);
+router.put('/updatepassword', protect, updatePassword);
 router.post('/forgotpassword', forgotPassword);
+router.put('/resetpassword/:resettoken', resetPassword);
 
 module.exports = router;
