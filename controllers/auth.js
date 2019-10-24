0d9a05ae93516849b0567d3d327188c79b5b7625
mario_nikolov <mnikolov@rnd-solutions.net>
2019-10-24 07:41:10 +0300

    add login user;

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,7 @@ const asyncHandler = require('../middleware/async');
 const ErrorResponse = require('../utils/errorResponse');
 
 // @desc    Register user
-// @route   GET /api/v1/auth/register
+// @route   POST /api/v1/auth/register
 // @access  Public
 exports.register = asyncHandler(async (req, res, next) => {
   const { name, email, password, role } = req.body;
@@ -17,5 +17,37 @@ exports.register = asyncHandler(async (req, res, next) => {
     role
   });
 
-  res.status(201).json({ success: true });
+  // Create token
+  const token = user.getSignedJwtToken();
+
+  res.status(201).json({ success: true, token });
+});
+
+// @desc    Login user
+// @route   POST /api/v1/auth/login
+// @access  Public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Validate email and password
+  if (!email || !password) {
+    return next(new ErrorResponse('Please provide email and password.', 400));
+  }
+
+  // Check for user
+  const user = await User.findOne({ email }).select('+password');
+  if (!user) {
+    return next(new ErrorResponse('Wrong email or password.', 401));
+  }
+
+  // Check if password matches
+  const isMatch = await user.matchPassword(password);
+  if (!isMatch) {
+    return next(new ErrorResponse('Wrong email or password.', 401));
+  }
+
+  // Create token
+  const token = user.getSignedJwtToken();
+
+  res.status(201).json({ success: true, token });
 });
